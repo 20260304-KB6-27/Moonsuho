1219259a61ae82e32dc138b865ebc774c77713eb
unknown <msh1593@naver.com>
2026-03-24 15:40:10 +0900

    Feat: 단일컴포넌트 실습 (0324)

---
+++ 03_Vue/01_basic/06_단일파일컴포넌트/test-vue-app/src/components/CheckboxItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li><input type="checkbox" v-model="checked" />옵션</li>
+</template>
+
+<!-- setup 안썼을 때 -->
+<!-- <script>
+import { ref } from 'vue';
+
+export default {
+  name: 'CheckboxItem',
+  setup() {
+    const checked = ref(false);
+    return { checked };
+  },
+};
+</script> -->
+<script setup>
+import { ref } from 'vue';
+const checked = ref(false);
+</script>
+
+<style lang="scss" scoped></style>
