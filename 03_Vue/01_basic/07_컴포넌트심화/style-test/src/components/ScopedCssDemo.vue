275af41030d1cdf91e5bcd743792931cc5882cdb
unknown <msh1593@naver.com>
2026-03-26 21:51:54 +0900

    Feat: 컴포넌트 심화 실습 (0325)

---
+++ 03_Vue/01_basic/07_컴포넌트심화/style-test/src/components/ScopedCssDemo.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>2. 범용 CSS (Scoped)</h2>
+  <div class="box">Scoped css가 적용된 박스입니다</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* 
+Scoped 속성
+- 이 컴포넌트에만 스타일 적용
+- Vue가 data-v해시값 속성을 생성하여 적용함.
+- 다른 컴포넌트의 같은 클래스명과 충돌하지 않음. */
+.box {
+  background-color: aqua;
+  border: 2px solid blue;
+}
+</style>
