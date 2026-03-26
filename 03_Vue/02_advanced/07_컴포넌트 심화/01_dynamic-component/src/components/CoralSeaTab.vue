ddaaf4dc02ad233c82f557eca965585818d4e854
unknown <msh1593@naver.com>
2026-03-26 16:36:50 +0900

    Fix: 실습에서 캐싱 작동 안한 문제 해결

--- 03_Vue/02_advanced/07_컴포넌트 심화/01_dynamic-component/src/components/CoralSeaTab.vue
+++ 03_Vue/02_advanced/07_컴포넌트 심화/01_dynamic-component/src/components/CoralSeaTab.vue
@@ -5,6 +5,12 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+//컴포넌트 이름 직접 지정
+//컴포넌트 파일 복붙하는 과정에서 내부적으로 꼬인 것 같아서, 직접 지정해줌
+defineOptions({
+  name: 'CoralSeaTab',
+});
+</script>
 
 <style scoped></style>
