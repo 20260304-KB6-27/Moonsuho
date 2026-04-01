07016ce6be08901e61e000cc44cf7693619a72e3
unknown <msh1593@naver.com>
2026-04-01 15:22:05 +0900

    Feat: 라우터 실습 (0401)

---
+++ 03_Vue/01_basic/09_Router/router-test/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+// vue 어플리케이셔에 router 등록
+app.use(router);
+
+app.mount('#app');
