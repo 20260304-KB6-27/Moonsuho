6e556ae3b78fc6b5479dfcd43d42f5b50d7a9dc6
unknown <msh1593@naver.com>
2026-04-02 15:38:51 +0900

    Feat: Axios실습 (0402)

--- 03_Vue/01_basic/09_Router/router-test/src/pages/MemberList.vue
+++ 03_Vue/01_basic/09_Router/router-test/src/pages/MemberList.vue
@@ -2,7 +2,7 @@
   <h1>Members</h1>
   <div v-for="m in members" :key="m.id">
     <RouterLink :to="{ name: 'members/id', params: { id: m.id } }">
-      <img :src="m.poto" style="width: 90px; height: 110px" />
+      <img :src="m.photo" style="width: 90px; height: 110px" />
       <h6>{{ m.name }}</h6>
     </RouterLink>
   </div>
