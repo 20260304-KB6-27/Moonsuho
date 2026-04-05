56932c330a11f927933ba5dc18404dcab8f0e9ba
unknown <msh1593@naver.com>
2026-04-05 16:46:15 +0900

    Feat: 라우터 실습

---
+++ 03_Vue/01_basic/11_Router_Axios/todolist/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: () => import('@/views/Home.vue') },
+    { path: '/about', component: () => import('@/views/About.vue') },
+    { path: '/todos', component: () => import('@/views/TodoList.vue') },
+    { path: '/todos/add', component: () => import('@/views/AddTodo.vue') },
+    { path: '/students', component: () => import('@/views/Students.vue') },
+    // (.*)/ 하위에 모든 문자열을 포함하게 되는 정규식
+    { path: '/:path(.*)', component: () => import('@/views/NotFound.vue') },
+  ],
+});
+
+export default router;
