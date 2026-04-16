78e287d966bb953b1bf1d3390c31803fb2ae021b
Moonsuho <msh1593@naver.com>
2026-04-16 14:56:02 +0900

    Feat:자바 클래스 실습 (0416)

---
+++ 04._Java/ch03-class/src/basic/ch06/sec00/book/Application.java
@@ -0,0 +1,19 @@
+package basic.ch06.sec00.book;
+
+public class Application {
+    public static void main(String[] args) {
+        //객체만들기
+        Book myBook = new Book(); //생성자호출
+        myBook.title = "해리포터와 마법사의 돌";
+        myBook.author = "조엔 k 롤링";
+        myBook.coverType = "하드커버";
+        myBook.pageCount = 320;
+
+        System.out.println(myBook);
+
+        Book myBook2 = new Book("조엔 k 롤링","해리포터와 혼혈왕자","소프트커버",320);
+
+        myBook2.introduce();
+        System.out.println(myBook2);
+    }
+}
---
+++ 04._Java/ch03-class/src/basic/ch06/sec11/exam02/Application.java
@@ -0,0 +1,14 @@
+package basic.ch06.sec11.exam02;
+
+public class Application {
+    public static void main(String[] args) {
+        //인스턴스를 안만들어도 사용 가능
+        System.out.println(Earth.EARTH_RADIUS);
+        System.out.println(Earth.EARTH_SURFACE_AREA);
+//        System.out.println(Earth);
+        Earth earth = new Earth();
+        System.out.println(earth.mainSatellite);
+
+    }
+}
+
