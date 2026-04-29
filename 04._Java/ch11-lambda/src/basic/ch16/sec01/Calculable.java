19219abdf8393e31cbca9c81fd1e4ec5a561ffb5
Moonsuho <msh1593@naver.com>
2026-04-29 15:51:30 +0900

    Feat: 자바실습과제(0429)

---
+++ 04._Java/ch11-lambda/src/basic/ch16/sec01/Calculable.java
@@ -0,0 +1,6 @@
+package basic.ch16.sec01;
+
+public interface Calculable {
+    //추상 메소드
+    void calculate(int x, int y);
+}
---
+++ 04._Java/ch11-lambda/src/basic/ch16/sec03/Speakable.java
@@ -0,0 +1,5 @@
+package basic.ch16.sec03;
+@FunctionalInterface
+public interface Speakable {
+    void speak(String content);
+}
